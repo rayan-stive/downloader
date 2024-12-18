4b0754394554bb7660dc7a4fc4101bd1c7b0d3fd
rayan <rayanstive9@gmail.com>
2024-12-18 13:45:27 +0300

    feat(downloader): Add interface graphique

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from pytube import YouTube
+from flask import Flask, render_template
 
-url = input('Entre un url : ')
 
-video = YouTube(url)
-stream = video.streams.get_highest_resolution()
+app = Flask(__name__)
 
-stream.download()
+# url = input('Entre un url : ')
 
-print('Video downloaded successfully !')
+# video = YouTube(url)
+# stream = video.streams.get_highest_resolution()
+
+# stream.download()
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
